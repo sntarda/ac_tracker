8aeee32db63e551fcaed947ed13fa9466bd5d302
Samir Tarda <115050984+sntarda@users.noreply.github.com>
2024-07-10 17:34:05 -0500

    Delete models directory

--- models/database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def init_db():
-    conn = sqlite3.connect('ac_tracker.db')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS units (id INTEGER PRIMARY KEY, location TEXT, manufacturer TEXT, model_number TEXT, serial_number TEXT, tonnage REAL, seer TEXT, heat TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY, unit_id INTEGER, issue TEXT, part TEXT, repair_status TEXT, date_repaired DATE, cost REAL)''')
-    conn.commit()
-    conn.close()
