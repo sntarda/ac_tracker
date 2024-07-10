a6022d1723cc558d785e2a6c033c3115446a80e8
Samir Tarda <115050984+sntarda@users.noreply.github.com>
2024-07-10 16:26:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from utils.auth import login, logout, check_authentication
+
+# Debugging prints
+print("Before importing pages")
 import pages.add_edit_unit
 import pages.add_ticket
 import pages.building_1001
 import pages.building_1055
 import pages.building_1057
 import pages.building_1059
+print("After importing pages")
 
 st.set_page_config(page_title="AC Unit Tracker", layout="wide")
 
@@ -26,16 +30,22 @@ if st.session_state['authenticated']:
         st.title("Home Page")
         st.write("Company Information: [Insert details here]")
     elif menu == "Add/Edit Unit":
+        print("Calling display_page for Add/Edit Unit")
         pages.add_edit_unit.display_page()
     elif menu == "Add Ticket":
+        print("Calling display_page for Add Ticket")
         pages.add_ticket.display_page()
     elif menu == "Building 1001":
+        print("Calling display_page for Building 1001")
         pages.building_1001.display_page()
     elif menu == "Building 1055":
+        print("Calling display_page for Building 1055")
         pages.building_1055.display_page()
     elif menu == "Building 1057":
+        print("Calling display_page for Building 1057")
         pages.building_1057.display_page()
     elif menu == "Building 1059":
+        print("Calling display_page for Building 1059")
         pages.building_1059.display_page()
 else:
     st.write("Please login to access the application.")
