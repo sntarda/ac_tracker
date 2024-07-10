418c02e25348548d895dcb34fd8f3e2e07612e6a
Samir Tarda <115050984+sntarda@users.noreply.github.com>
2024-07-10 14:55:43 -0500

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,1 +1,14 @@
+from streamlit_authenticator import Authenticate
 
+credentials = {
+    'usernames': {
+        'admin': {'password': 'password'}
+    }
+}
+
+authenticator = Authenticate(
+    credentials=credentials,
+    cookie_name="ac_tracker",
+    key="some_random_key",
+    cookie_expiry_days=1,
+)
