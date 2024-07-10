f2b7e2efb9f966fea42a3190851e91f35d1ae670
Samir Tarda <115050984+sntarda@users.noreply.github.com>
2024-07-10 17:44:06 -0500

    Update db_operations.py

--- utils/db_operations.py
+++ utils/db_operations.py
@@ -8,3 +8,10 @@ def load_units(file_path):
 def save_units(file_path, units_df):
     units_df.to_csv(file_path, index=False)
 
+# Function to load tickets from a CSV file
+def load_tickets(file_path):
+    return pd.read_csv(file_path)
+
+# Function to save tickets to a CSV file
+def save_tickets(file_path, tickets_df):
+    tickets_df.to_csv(file_path, index=False)
